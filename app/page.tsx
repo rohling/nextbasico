2d34529e52a7f90944e6f7cbd4fa51dfc0946f50
Adair Rohling <rohling@users.noreply.github.com>
2026-05-06 23:19:41 +0000

    fix: config

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Home() {
+
+  const a= 12;
+  return (
+    <>
+      <h1 className="text-center bg-amber-400" >UTFPR</h1>
+      <h2 className="text-center">Francisco Beltrão</h2>
+      <HomeConent/>
+    </>
+  );
+}
+export  function HomeConent(){
+  return(
+    <h1>Conteúdo</h1>
+  )
+}
+
